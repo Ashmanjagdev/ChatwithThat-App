cc5abad554f6d5c1fbf51c24017a5a611715ea3e
Ashman jagdev <88061051+Ashmanjagdev@users.noreply.github.com>
2023-01-29 00:36:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
 }
 
-app.listen(4000,()=>{
+app.listen(5000,()=>{
     console.log(`Working`);
 });
 
