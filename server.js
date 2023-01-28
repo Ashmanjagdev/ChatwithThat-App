467da18ea71d1ee4963659a68cf59fe6a26e4b97
Ashman jagdev <88061051+Ashmanjagdev@users.noreply.github.com>
2023-01-29 00:24:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const passport=require("passport");
 const passportLocalMongoose=require("passport-local-mongoose");
 mongoose.set('strictQuery', false);
 
+const http=require("http");
 const cors = require("cors");
+const socketIO = require("socket.io");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -46,6 +48,9 @@ passport.deserializeUser(User.deserializeUser());
 
 const users=[{}];
 
+app.get("/",(req,res)=>{
+    res.send("HELL ITS WORKING");
+});
 
 var values='';
 app.get("/check", (req, res) => {
@@ -53,7 +58,7 @@ app.get("/check", (req, res) => {
 
   if(req.isAuthenticated()){
     var name=req.user.name;
-    
+
     const myJSON = JSON.stringify(name);
       return res.send(myJSON);
   }
@@ -111,10 +116,10 @@ app.post('/signup', (req, res) => {
 
       if (items2.username) {
          return res.status(393).json({error:"email id already exist"});
-        
+
       }
       else{
-        
+
          if (req.body.password!=req.body.confirmpassword) {
            return res.status(391).json({error:"passwords are not matching"});
 
@@ -125,12 +130,12 @@ app.post('/signup', (req, res) => {
               return res.status(392).json({error:"email id already exist"});
             }
             else{
-                
+
             passport.authenticate("local")(req,res, function(){
             return res.send("200");
-            }); 
+            });
         }
-        
+
         });
 
       }
@@ -152,4 +157,61 @@ app.listen(4000,()=>{
 
 
 
+const server=http.createServer(app);
 
+const io=socketIO(server);
+
+io.on("connection",(socket)=>{
+    socket.on('joined',({values})=>{
+        if(values!="")
+        {
+users[socket.id]=values;
+          console.log(`${values} has joined `);
+          socket.join(221);
+          socket.to(221).emit('userJoined',{user:"Global",message:` ${users[socket.id]} has joined`});
+          socket.emit('welcome',{user:"Global",message:`Welcome to the chat, ${users[socket.id]} `})
+        }
+    });
+var room1=221;
+	socket.on("message",({message,id,room}) => {
+        if(room === ""){
+            socket.join(221);
+            io.to(221).emit('sendMessage',{user:users[id],message,id});
+        }
+        else{
+            room1=room;
+            io.to(room).emit('sendMessage',{user:users[id],message,id});
+        }
+	});
+    var prevroom=221;
+
+    socket.on("room-joined",({room,id}) => {
+        if(room === ""){
+            socket.join(221);
+            prevroom=221;
+        }
+        else{
+            socket.leave(prevroom);
+            io.to(prevroom).emit('leave',{user:"Global",message:`${users[socket.id]}  has left`});
+            socket.join(room);
+            socket.to(room).emit('entered-room',{user:"Global",message:` ${users[socket.id]} has joined the ${room}`});
+            socket.emit('room-filled',{user:"Global",message:`you joined room ${room} `})
+            prevroom=room;
+        }
+    });
+
+	socket.on("disconnect",() => {
+        if(users[socket.id]){
+        io.to(room1).emit('leave',{user:"Global",message:`${users[socket.id]}  has left`});
+        console.log(`user left`);
+        }
+	});
+
+
+});
+
+
+
+server.listen( 5000,()=>{
+    console.log(`Working`);
+});
